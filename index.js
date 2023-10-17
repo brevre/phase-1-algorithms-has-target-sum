c5c7d664b526154ae3c9c63a9283a455a99b218e
brevre <brendamakena09@gmail.com>
2023-10-17 10:24:18 +0200

    fin

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-function hasTargetSum(array, target) {
+
   // Write your algorithm here
+
+function hasTargetSum(array, target) {
+  // Create a set to store numbers we've seen so far
+  const seenNumbers = new Set();
+
+  // Iterate through the array
+  for (let number of array) {
+    // Calculate the complement (the number needed to reach the target)
+    const complement = target - number;
+
+    // If the complement is in the set, we found a pair that adds up to the target
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+
+    // Add the current number to the set
+    seenNumbers.add(number);
+  }
+
+  // No pair was found that adds up to the target
+  return false;
 }
+
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // Expecting: true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));   // Expecting: true
+console.log(hasTargetSum([1, 2, 5], 4));             // Expecting: false
 
 /* 
   Write the Big O time complexity of your function here
